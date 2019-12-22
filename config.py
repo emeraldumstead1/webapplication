7f7965772d783419ad1de9168afd55410839b449
emeraldumstead1 <edu2@njit.edu>
2019-12-22 08:48:27 -0500

    Task: Added testing to config.py

--- config.py
+++ config.py
@@ -24,3 +24,22 @@ app_config = {
     'development': Development,
     'production': Production,
 }
+
+#####################
+ # existing code here#
+ #####################
+ class Testing(object):
+    """
+    Development environment configuration
+    """
+    TESTING = True
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_TEST_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+
+ app_config = {
+  #####################
+    # existing code here#
+    #####################
+    'testing': Testing
+ }
